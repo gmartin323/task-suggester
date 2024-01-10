00fa2a020d2be0436948e24c1bb57ce3f54d080b
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-01-10 17:28:00 +0000

    initial project setup

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "https://kit.fontawesome.com/6b203b1712.js"
+import "./index.css"
+import App from "./App"
+
+const DOM = ReactDOM.createRoot(document.getElementById("root"));
+DOM.render(
+    <React.StrictMode>
+            <App />
+    </React.StrictMode>
+);
