a8622d9d52d4245460d58c90c0299a79969f8a08
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-01-10 18:13:59 +0000

    Check netlify/github deployment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
       <div className="App">
-        <h1>Hello React <i className="fa-solid fa-handshake"></i></h1>
+        <h1>Task Suggester</h1>
       </div>
   );
 }
