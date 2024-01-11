e27665dfaa227f44ddc585f712aca81d3dd442ec
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-01-11 14:51:09 +0000

    Can now add to and display from firebase database

--- src/pages/todo.jsx
+++ src/pages/todo.jsx
@@ -8,28 +8,64 @@ import {
 } from "firebase/firestore"
 import { todoCollection } from "../firebase";
 
+// To-Do //
+
+// I. Save to database collection in addTodo function
+// II. Each item needs: 
+        // a. text content
+        // b. createdAt time using Date.now()
+        // c. updatedAt time using Date.now()
+        // d. isCompleted: true/false -> toBeChanged when user clicks completed button
+        // e. id: -> in order to identify when deleting/editing
+// III. Get data from database when page loads
+// IV. Delete from database when user compltes 'delete' action (icon click? / menu selection? / swipe and click icon?)  
+// V. Allow user to edit todos in place -> property (contentEditable) on p element? ->
+
 export default function Todo() {
-  const [currentTodo, setCurrentTodo] = React.useState("")
+  const [currentTodoText, setCurrentTodoText] = React.useState("")
   const [todos, setTodos] = React.useState([])
+
+  React.useEffect(() => {
+    const unsubscribe = onSnapshot(todoCollection, function(snapshot) {
+      const todoArr = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }))
+      setTodos(todoArr)
+    })
+    return unsubscribe
+  }, [])
+
+  console.log(todos)
 
   const todosEl = todos.map((todo) => {
     return (
-      <p>{todo}</p>
+      <p /* contentEditable="true" */>{todo.text}</p>
     )
   })
 
-  function updateCurrentTodo(event) {
-    setCurrentTodo(event.target.value)
+  function updateCurrentTodoText(event) {
+    setCurrentTodoText(event.target.value)
   }
 
   function addTodo() {
     setTodos((prevTodos) => {
-      return [...prevTodos, currentTodo] //add new ... to end of array
+      return [...prevTodos, currentTodoText]
     })
-    setCurrentTodo("")
+    setCurrentTodoText("")
   }
 
-  console.log(todos)
+  async function addNewTodo() {
+    const newTodo = {
+      text: currentTodoText,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      isCompleted: false,
+      // firebase will set an id when creating the document
+    }
+    await addDoc(todoCollection, newTodo)
+    setCurrentTodoText("")
+  }
 
   return (
     <div className="page-container">
@@ -41,11 +77,11 @@ export default function Todo() {
         type="text"
         placeholder="Type here"
         name="new-todo-input"
-        value={currentTodo}
-        onChange={updateCurrentTodo}
-        onKeyDown={() => {(event.key === 'Enter') ? addTodo() : null}}
+        value={currentTodoText}
+        onChange={updateCurrentTodoText}
+        onKeyDown={() => {(event.key === 'Enter') ? addNewTodo() : null}}
       />
-      <button onClick={addTodo}> Add + </button>
+      <button onClick={addNewTodo}> Add + </button>
     </div>
   )
 }
